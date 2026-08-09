f0a04237e2958d3b647a0c2384605533d6e9aeb6
diwakarprabhu <diwakar005007@gmail.com>
2026-08-09 17:24:49 +0530

    App.css display grid added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
   return (
     <>
     
-    <div className="grid grid-cols-4 gap-6 p-6 ">
+    <div className="grid grid-cols-4 gap-6 p-6 products- ">
        
       {productData.map((data) => (
         <ProductCard
